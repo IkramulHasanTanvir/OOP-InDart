228ea2250520a671e2fa0c9b1dda138397a5d79d
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-06-15 06:33:19 +0600

    update

--- class_example/animal.dart
+++ class_example/animal.dart
@@ -2,7 +2,6 @@ class Animal {
   String? name;
   int? numberOfLegs;
   int? lifeSpanYear; //properties
-
   void Display() {
     print('Animal name is : $name');
     print('Animal legs is : $numberOfLegs');
