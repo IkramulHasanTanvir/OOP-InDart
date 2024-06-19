73aceebe64d1e3597de108fa66ff485ceee60210
ikramulhasantanvir <ikramulhasantanvir@gmail.com>
2024-06-19 18:15:29 +0600

    static, emun

---
+++ Static_or_enum/static.dart
@@ -0,0 +1,24 @@
+class Employee {
+  // Static variable
+  static int count = 0;
+
+  // Constructor
+  Employee() {
+    count++;
+  }
+
+  // Method to display the value of count
+  void totalEmployee() {
+    print("Total Employee: $count");
+  }
+}
+
+void main() {
+  // Creating objects of Employee class
+  Employee tanvir = Employee();
+  tanvir.totalEmployee();
+  Employee hridoy = Employee();
+  hridoy.totalEmployee();
+  Employee rafat = Employee();
+  rafat.totalEmployee();
+}
